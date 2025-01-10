7877111696c51a6239b62583a109aa0bff9f444c
ThangKa <hothang2262004@gmail.com>
2025-01-10 20:57:57 +0700

    Day-10/1

--- src/main/java/com/example/JobHunter/controller/admin/HomeController.java
+++ src/main/java/com/example/JobHunter/controller/admin/HomeController.java
@@ -3,6 +3,7 @@ package com.example.JobHunter.controller.admin;
 import org.springframework.web.bind.annotation.GetMapping;
 
 public class HomeController {
+
     @GetMapping("/")
     public String geString() {
         return "Hello everyone";
--- src/main/java/com/example/JobHunter/domain/User.java
+++ src/main/java/com/example/JobHunter/domain/User.java
@@ -4,8 +4,14 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
+@Table(name = "users")
+@Getter
+@Setter
 public class User {
 
     @Id
@@ -15,35 +21,4 @@ public class User {
     private String email;
     private String password;
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
 }
