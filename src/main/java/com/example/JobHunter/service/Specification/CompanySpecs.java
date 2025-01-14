7fc3db7c98048d0b216016f65de1d43beb4be332
ThangKa <hothang2262004@gmail.com>
2025-01-14 23:57:42 +0700

    upadte

--- src/main/java/com/example/JobHunter/service/Specification/CompanySpecs.java
+++
@@ -1,9 +0,0 @@
-package com.example.JobHunter.service.Specification;
-
-import org.springframework.data.jpa.domain.Specification;
-
-import com.example.JobHunter.domain.Company;
-
-public class CompanySpecs {
-
-}
